d323a31f50b121db801b0a113a019647e8ca7eb4
Joaquin Negrete <joaquin.negrete03@gmail.com>
2024-09-15 12:17:34 +0200

    Add dash property and getters

--- src/player.py
+++ src/player.py
@@ -1,4 +1,17 @@
 class Player:
   def __init__(self, number: int, initial_position: tuple[int, int]):
     self.__number: int = number
-    self.__position: tuple = initial_position
+    self.__position: tuple[int, int] = initial_position
+    self.__dash: list[tuple[int, int]] = [initial_position]
+
+  @property
+  def number(self) -> int:
+    return self.__number
+
+  @property
+  def position(self) -> tuple[int, int]:
+    return self.__position
+
+  @property
+  def dash(self) -> list[tuple[int, int]]:
+    return self.__dash

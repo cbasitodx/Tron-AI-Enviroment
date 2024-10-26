73ce789f2574e7059eccd9410f910dbfee1f62ee
Joaquin Negrete <joaquin.negrete03@gmail.com>
2024-10-26 14:55:16 +0200

    Add previous_move and delete dash

--- src/player.py
+++ src/player.py
@@ -2,7 +2,7 @@ class Player:
   def __init__(self, number: int, initial_position: tuple[int, int]):
     self.__number: int = number
     self.__position: tuple[int, int] = initial_position
-    self.__dash: list[tuple[int, int]] = [initial_position]
+    self.__previous_move: int = 0
 
   @property
   def number(self) -> int:
@@ -13,5 +13,5 @@ class Player:
     return self.__position
 
   @property
-  def dash(self) -> list[tuple[int, int]]:
-    return self.__dash
+  def previous_move(self) -> int:
+    return self.__previous_move

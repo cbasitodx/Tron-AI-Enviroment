d5738c5190c41cd253dcf47d974a3134f41f6511
Joaquin Negrete <joaquin.negrete03@gmail.com>
2024-11-09 17:47:25 +0100

    Fix initial random position after adding wall borders

--- src/player.py
+++ src/player.py
@@ -22,12 +22,37 @@ class Player:
     return self.__previous_move
 
   def __generate_initial_position(self, size: int) -> tuple[int, int]:
+    """
+    Generate the random initial position for the player
+
+    The board looks like this:
+
+        +---+---+---+---+---+---+
+     0  | # | # | # |...| # | # |
+        +---+---+---+---+---+---+
+     1  | # |   | 2 | 2 | 2 | # |
+        +---+---+---+---+---+---+
+     2  | # | 1 |   | 2 | 2 | # |
+        +---+---+---+---+---+---+
+    ... |...| 1 | 1 |   | 2 |...|
+        +---+---+---+---+---+---+
+    n-2 | # | 1 | 1 | 1 |   | # |
+        +---+---+---+---+---+---+
+    n-1 | # | # | # |...| # | # |
+        +---+---+---+---+---+---+
+          0   1   2  ... n-1 n-2
+
+    :param size: The size of the board
+    :type size: int
+    :return: The initial position of the player
+    :rtype: tuple[int, int]
+    """
     if self.__number == PLAYER_1:
-      x = randint(1, size - 1)
+      x = randint(1, size - 3)
       y = randint(0, x - 2)
 
     elif self.__number == PLAYER_2:
-      y = randint(1, size - 1)
+      y = randint(1, size - 3)
       x = randint(1, y - 2)
 
     else:

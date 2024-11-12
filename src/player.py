4c2621f8e5dd6ed49c7a9a50c141cf2e1766049a
Joaquin Negrete <joaquin.negrete03@gmail.com>
2024-11-12 16:35:19 +0100

    Fix the random start position of the player
    Fix it to take into account the wall borders.

--- src/player.py
+++ src/player.py
@@ -6,7 +6,7 @@ from .consts import PLAYER_1, PLAYER_2
 class Player:
   def __init__(self, number: int, size: int):
     self.__number: int = number
-    self.__position: tuple[int, int] = self.__generate_initial_position(size)
+    self.__position: list[tuple[int, int]] = [self.__generate_initial_position(size)] + [(0, 0)] * 5
     self.__previous_move: int = 0  # 0 means no previous move
 
   @property
@@ -22,13 +22,34 @@ class Player:
     return self.__previous_move
 
   def __generate_initial_position(self, size: int) -> tuple[int, int]:
+    """
+    This function generates the initial position of the player.
+    The posible positions are the following:
+
+          0   1   2  ... n-2 n-1
+        +---+---+---+---+---+---+
+     0  | # | # | # |...| # | # |
+        +---+---+---+---+---+---+
+     1  | # |   | 1 | 1 | 1 | # |
+        +---+---+---+---+---+---+
+     2  | # | 2 |   | 1 | 1 | # |
+        +---+---+---+---+---+---+
+    ... |...| 2 | 2 |   | 1 |...|
+        +---+---+---+---+---+---+
+    n-2 | # | 2 | 2 | 2 |   | # |
+        +---+---+---+---+---+---+
+    n-1 | # | # | # |...| # | # |
+        +---+---+---+---+---+---+
+
+    Where the # is a wall, 1 is player 1 and 2 is player 2.
+    """
     if self.__number == PLAYER_1:
-      x = randint(1, size - 1)
-      y = randint(0, x - 2)
+      x = randint(2, size - 3)
+      y = randint(1, x - 1)
 
     elif self.__number == PLAYER_2:
-      y = randint(1, size - 1)
-      x = randint(1, y - 2)
+      y = randint(2, size - 3)
+      x = randint(1, y - 1)
 
     else:
       raise InvalidPlayerNumberError(f"Invalid player number: {self.__number}")

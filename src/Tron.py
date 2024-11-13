2b80e26b1aa31f474c85a6968d9bc9d8a88e3163
Joaquin Negrete <joaquin.negrete03@gmail.com>
2024-11-14 00:05:02 +0100

    Make `Tron` more single responsability
    Added classes `Board` and `Move` to handle the board creation and
    updates, and to handle move validation and position calculation,
    respectively.

--- src/Tron.py
+++ src/Tron.py
@@ -3,21 +3,16 @@ import asyncio
 from src.consts import *
 from src.player import Player
 from src.input import get_input
+from src.move import Move
+from src.board import Board
 
 
 class Tron:
   def __init__(self, size: int):
-    # Dimensions of the board
-    self.__size: int = size
-
-    # Players
     self.__player_1: Player = Player(PLAYER_1, size)
     self.__player_2: Player = Player(PLAYER_2, size)
 
-    # Game board. 0 means empty, 1 means player 1, 2 means player 2
-    self.__board: list[list[int]] = [[0] * size for _ in range(size)]
-    self.__walls: set[tuple[int, int]] = set()
-    self.__init_walls()
+    self.__board = Board(size, self.__player_1, self.__player_2)
 
     # Add the initial positions of the players to the board
     self.__board[self.__player_1.position[0][0]][self.__player_1.position[0][1]] = PLAYER_1
@@ -29,50 +24,15 @@ class Tron:
     # Winner of the game
     self.__winner: Player | None = None
 
-  def __init_walls(self) -> None:
-    n = self.__size - 1
-    for i in range(self.__size):
-      self.__walls.add((0, i))
-      self.__walls.add((self.__size - 1, i))
-      self.__walls.add((i, 0))
-      self.__walls.add((i, self.__size - 1))
-
-      self.__board[0][i] = WALL
-      self.__board[n][i] = WALL
-      self.__board[i][0] = WALL
-      self.__board[i][n] = WALL
-
-  def __is_valid_move(self, move: int) -> bool:
-    if not isinstance(move, int):
-      return False
-
-    if 1 <= move <= 4:
-      return True
-
-    return False
-
   async def __get_moves(self) -> tuple[tuple[int, int], tuple[int, int]]:
     move_1, move_2 = await asyncio.gather(get_input(self.__player_1.number), get_input(self.__player_2.number))
-    if not self.__is_valid_move(move_1) or self.__player_1.player_suicided(move_1):
+    if not Move.is_valid_move(move_1) or self.__player_1.player_suicided(move_1):
       move_1 = self.__player_1.previous_move
 
-    if not self.__is_valid_move(move_2) or self.__player_2.player_suicided(move_2):
+    if not Move.is_valid_move(move_2) or self.__player_2.player_suicided(move_2):
       move_2 = self.__player_2.previous_move
 
     return move_1, move_2
-
-  def __get_new_position(self, player: Player, move: int) -> tuple[int, int]:
-    match move:
-      case 1:  # Move left
-        new_pos = (player.position[0][0], player.position[0][1] - 1)
-      case 2:  # Move up
-        new_pos = (player.position[0][0] - 1, player.position[0][1])
-      case 3:  # Move right
-        new_pos = (player.position[0][0], player.position[0][1] + 1)
-      case 4:  # Move down
-        new_pos = (player.position[0][0] + 1, player.position[0][1])
-
-    return new_pos
 
   def __get_collision(self, player_1: Player, player_2: Player) -> int | None:
     """
@@ -97,10 +57,6 @@ class Tron:
 
     return None
 
-  def __move_player(self, player: Player, move: int) -> None:
-    new_pos = self.__get_new_position(player, move)
-    player.move(new_pos, move)
-
   def __handle_collisions(self) -> str | None:
     collision = self.__get_collision(self.__player_1, self.__player_2)
 
@@ -109,7 +65,7 @@ class Tron:
     if collision is not None:
       if collision == PLAYERS_COLLIDED:
         self.__winner = None
-        end_message = "Both players collided into each other"
+        end_message = "Players collided into each other"
       elif collision == BOTH_WALLS:
         self.__winner = None
         end_message = "Both players collided into a wall"
@@ -120,18 +76,6 @@ class Tron:
       self.__game_over = True
 
     return end_message
-
-  def __update_board(self, last_pos_1: tuple[int, int] | None, last_pos_2: tuple[int, int] | None) -> None:
-    for pos_1, pos_2 in zip(self.__player_1.position, self.__player_2.position):
-      if pos_1 is None or pos_2 is None:
-        break
-      self.__board[pos_1[0]][pos_1[1]] = PLAYER_1
-      self.__board[pos_2[0]][pos_2[1]] = PLAYER_2
-
-      if last_pos_1 is None or last_pos_2 is None:
-        break
-      self.__board[last_pos_1[0]][last_pos_1[1]] = 0
-      self.__board[last_pos_2[0]][last_pos_2[1]] = 0
 
   async def play(self) -> None:
     self.print_board()
@@ -145,8 +89,8 @@ class Tron:
       last_pos_2 = self.__player_2.position[-1]
 
       # Move the players
-      self.__move_player(self.__player_1, move_1)
-      self.__move_player(self.__player_2, move_2)
+      Move.move_player(self.__player_1, move_1)
+      Move.move_player(self.__player_2, move_2)
 
       # Check for collisions
       end_message = self.__handle_collisions()
@@ -154,7 +98,7 @@ class Tron:
         break
 
       # Update the matrix
-      self.__update_board(last_pos_1, last_pos_2)
+      self.__board.update_board(last_pos_1, last_pos_2)
 
       # Print the matrix
       self.print_board()
---
+++ src/move.py
@@ -0,0 +1,29 @@
+from src.player import Player
+
+
+class Move:
+  @staticmethod
+  def move_player(player: Player, move: int) -> None:
+    new_pos = Move.__get_new_position(player, move)
+    player.move(new_pos, move)
+
+  @staticmethod
+  def __get_new_position(player: Player, move: int) -> tuple[int, int]:
+    match move:
+      case 1:  # Move left
+        new_pos = (player.position[0][0], player.position[0][1] - 1)
+      case 2:  # Move up
+        new_pos = (player.position[0][0] - 1, player.position[0][1])
+      case 3:  # Move right
+        new_pos = (player.position[0][0], player.position[0][1] + 1)
+      case 4:  # Move down
+        new_pos = (player.position[0][0] + 1, player.position[0][1])
+
+    return new_pos
+
+  @staticmethod
+  def is_valid_move(move: int) -> bool:
+    if not isinstance(move, int):
+      return False
+
+    return 1 <= move <= 4
